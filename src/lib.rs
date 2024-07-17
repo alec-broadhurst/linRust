021a5a36d1d5e544644d5fd57471351840678c9b
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-07-17 15:58:40 -0500

    added get function for flatten matrix

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,14 @@ pub struct Matrix<T> {
     cols: usize,
     values: Vec<T>,
 }
+
+impl<T> Matrix<T> {
+    pub fn get(&self, row: usize, col: usize) -> Option<&T> {
+        if row < self.rows && col < self.cols {
+            let index = (row * self.cols) + row;
+            Some(&self.values[index])
+        } else {
+            None
+        }
+    }
+}
