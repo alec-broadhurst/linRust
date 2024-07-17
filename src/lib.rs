00626258ddda6be8bd3ebe3f40c47c95895145f4
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-07-17 14:50:29 -0500

    created Matrix struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,5 @@
-pub enum DType {
-    Int(i32),
-    UInt(u32),
-    Long(i64),
-    ULong(u64),
-    Float(f32),
-    Double(f64),
-}
-
-pub struct Matrix {
+pub struct Matrix<T> {
     rows: usize,
     cols: usize,
-    dtype: DType,
-    values: Vec<DType>,
-}
-
-impl Matrix {
-    fn new(rows: usize, cols: usize, dtype: DType, values: Vec<DType>) -> Self {
-        Matrix {
-            rows,
-            cols,
-            dtype,
-            values,
-        }
-    }
+    values: Vec<T>,
 }
