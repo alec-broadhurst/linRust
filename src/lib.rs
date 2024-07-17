ea4573172a3858817f5e886916152967c7db3bf3
Alec Broadhurst <alec@Alecs-MacBook-Air.local>
2024-07-17 17:04:17 -0500

    added test for indexing and fixed get index formula

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,32 @@ pub struct Matrix<T> {
 impl<T> Matrix<T> {
     pub fn get(&self, row: usize, col: usize) -> Option<&T> {
         if row < self.rows && col < self.cols {
-            let index = (row * self.cols) + row;
+            let index = (row * self.cols) + col;
             Some(&self.values[index])
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Matrix;
+
+    #[test]
+    fn check_indexing() {
+        let matrix: Matrix<u32> = Matrix {
+            rows: 3,
+            cols: 3,
+            values: vec![1, 2, 3, 4, 5, 6, 7, 8, 9],
+        };
+
+        assert_eq!(matrix.get(0, 0), Some(&1));
+        assert_eq!(matrix.get(1, 1), Some(&5));
+        assert_eq!(matrix.get(2, 0), Some(&7));
+
+        assert_eq!(matrix.get(3, 0), None);
+        assert_eq!(matrix.get(0, 3), None);
+        assert_eq!(matrix.get(3, 3), None);
     }
 }
